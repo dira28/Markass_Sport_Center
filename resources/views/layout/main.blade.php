fd94dd24f472d828b44a8f6dd61a1e6e809a0ccf
dira28 <askadira@gmail.com>
2026-03-03 08:22:27 +0700

    first commit

---
+++ resources/views/layout/main.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Markas Sport Center</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+
+<nav class="navbar navbar-dark bg-dark px-3">
+    <span class="navbar-brand">Markas Sport Center</span>
+</nav>
+
+<div class="container mt-4">
+    @yield('content')
+</div>
+
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('auth.login');
+})->name('login');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
+
