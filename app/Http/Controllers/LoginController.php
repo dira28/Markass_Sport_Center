428ad4830aaf7bf2cf7c7f320f8f911024670c53
ozorozor <ozorozor1337@gmail.com>
2026-07-21 02:59:37 +0700

    [OZ] fix everything :)

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -10,7 +10,7 @@ class LoginController extends Controller
     public function login(Request $request)
     {
         $request->validate([
-            'email' => 'required|email',
+            'email' => 'required|email' ,
             'password' => 'required'
         ]);
 
